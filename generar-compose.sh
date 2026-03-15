36528aa88c624ac9be4bed95fdc4afa6d94773cf
mreimundo <mreimundo@fi.uba.ar>
2026-03-14 23:15:43 -0300

    iteracion 2: modifico el formato para ver que me lo tomen los tests

--- generar-compose.sh
+++ generar-compose.sh
@@ -11,42 +11,39 @@ file_out=$1
 clients_qty=$2
 
 # creamos el header para el compose y se lo insertamos al archivo de salida
-cat > "$file_out" <<EOL
-name: tp0
+cat > "$file_out" <<EOF
 services:
   server:
     container_name: server
     image: server:latest
     entrypoint: python3 /main.py
     environment:
-      - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
+      PYTHONUNBUFFERED: 1
+      LOGGING_LEVEL: DEBUG
     networks:
       - testing_net
-EOL
+EOF
 
 # le sumo al archivo de salida la config por client
-i=1
-while [ $i -le $clients_qty ]; do
-  cat >> "$file_out" <<EOL
+for ((i=1;i<=clients_qty;i++)); do
+cat >> "$file_out" <<EOF
 
   client$i:
     container_name: client$i
     image: client:latest
     entrypoint: /client
     environment:
-      - CLI_ID=$i
-      - CLI_LOG_LEVEL=DEBUG
+      CLI_ID: $i
+      CLI_LOG_LEVEL: DEBUG
     networks:
       - testing_net
     depends_on:
       - server
-EOL
-  i=$((i + 1))
+EOF
 done
 
 # le agrego al archivo de salida la parte de red
-cat >> "$file_out" <<EOL
+cat >> "$file_out" <<EOF
 
 networks:
   testing_net:
@@ -54,6 +51,6 @@ networks:
       driver: default
       config:
         - subnet: 172.25.125.0/24
-EOL
+EOF
 
 echo "Archivo Docker Compose generado en: $file_out"
