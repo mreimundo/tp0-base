984ea5a0fc86cd3879ae6a22a498795eb071865f
mreimundo <mreimundo@fi.uba.ar>
2026-03-20 18:28:48 -0300

    ej6: update de volumes y quito env vars en cliente del bash

--- generar-compose.sh
+++ generar-compose.sh
@@ -42,13 +42,9 @@ cat >> "$file_out" <<EOF
     entrypoint: /client
     environment:
       CLI_ID: $i
-      NOMBRE: "Nombre$i"
-      APELLIDO: "Apellido$i"
-      DOCUMENTO: "3090446$i"
-      NACIMIENTO: "1990-01-0$i"
-      NUMERO: "757$i"
     volumes:
       - ./client/config.yaml:/config.yaml
+      - ./.data/agency-$i.csv:/agency.csv
     networks:
       - testing_net
     depends_on:
